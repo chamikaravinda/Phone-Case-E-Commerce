0ed16cfa9bdb25f26b0121694992b53a2fb9fd0b
Chamika Ravinda <chamikaravinda@gmail.com>
2020-07-29 13:46:22 +0530

    |Shooping Cart| Shooping cart page edited

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,55 +9,55 @@ import LoadingWindow from "./LoadingWindow";
 import AppleProducts from "./productComponents/AppleProducts";
 
 export default function Content(config, pageNavigation) {
-    const state = config.config
+  const state = config.config;
 
-    if (state.page === 0) {
-        return (
-            <div>
-                <HomePage/>
-            </div>
-        )
-    }
-    if (state.page === 1) {
-        return (
-            <div>
-                <Login config={config}/>
-            </div>
-        )
-    }
-    if (state.page === 2) {
-        return (
-            <div>
-                <SignUP/>
-            </div>
-        )
-    }
-    if (state.page === 3) {
-        return (
-            <div>
-                <ShoppingCart config={config}/>
-            </div>
-        )
-    }
-    if (state.page === 4) {
-        return (
-            <div>
-                <ReviewAndDescription/>
-            </div>
-        )
-    }
-
-    if (state.page === 5) {
-        return (
-            <div>
-                <AppleProducts/>
-            </div>
-        )
-    }
-
+  if (state.page === 0) {
     return (
-        <div>
-            <LoadingWindow/>
-        </div>
+      <div>
+        <HomePage />
+      </div>
     );
+  }
+  if (state.page === 1) {
+    return (
+      <div>
+        <Login config={config} />
+      </div>
+    );
+  }
+  if (state.page === 2) {
+    return (
+      <div>
+        <SignUP />
+      </div>
+    );
+  }
+  if (state.page === 3) {
+    return (
+      <div>
+        <ShoppingCart config={config} />
+      </div>
+    );
+  }
+  if (state.page === 4) {
+    return (
+      <div>
+        <ShippingAndPayment />
+      </div>
+    );
+  }
+
+  if (state.page === 5) {
+    return (
+      <div>
+        <AppleProducts />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <LoadingWindow />
+    </div>
+  );
 }
